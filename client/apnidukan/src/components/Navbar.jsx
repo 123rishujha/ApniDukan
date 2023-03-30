bb77bb7d682a3108117b81b0216f1da74ac434c0
sristy verma <sristyel2016@gmail.com>
2023-03-30 21:55:33 +0530

    nav footer comp with responsiveness

---
+++ client/apnidukan/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React ,{useState}from 'react'
+import './Navbar.css'
+import logo from './Apni_dukan_logo.png'
+import {Link} from 'react-router-dom'
+import { Badge } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { FaShoppingCart } from 'react-icons/fa';
+import { HamburgerIcon } from "@chakra-ui/icons";
+import SideMenuModal from './SideMenuModal'
+
+const Navbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
+  return (
+   <header className='header'>
+    <nav className='nav'>
+      <div className="leftnav">
+        <div className='hamicon'onClick={toggleModal}>
+          <HamburgerIcon w={9} h={9} color='grey' backgroundColor={'#131921'}/>
+          {isModalOpen && <SideMenuModal onClose={toggleModal} />}
+        </div>
+        <div className="logonav">
+          <img src={logo} alt='logomissing'/>
+        </div>
+        <div className="navinput">
+          <input type='text' name='' placeholder='Search Amazon.in'/>
+          <div className="navsearch">
+<SearchIcon w={8} h={8} className='navsearchicon'/>
+          </div>
+
+        </div>
+      </div>
+<div className="rightnav">
+  <div className="navsignin">
+    <Link to='/login'>Sign-In</Link>
+  </div>
+  
+  <div className="navcart">
+  <Badge colorScheme="black.900" w={"60%"} h={"80%"}>
+      <FaShoppingCart className='carticon'  style={{ width: "100%", height: "100%" }} />
+    </Badge>
+    <p>Cart</p>
+  </div>
+
+</div>
+    </nav>
+   </header>
+  )
+}
+
+export default Navbar
