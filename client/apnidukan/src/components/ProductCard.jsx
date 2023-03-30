52b2f673b6ab59e67924f89c13ec706b02afa53a
rsrajput03 <rajputrohit9560@gmail.com>
2023-03-30 18:35:00 +0530

    Product Page

--- client/apnidukan/src/components/ProductCard.jsx
+++ client/apnidukan/src/components/ProductCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "./product.module.css";
 
-const ProductCard = ({ image, title, old_price, price, discount, rating }) => {
+const ProductCard = ({ image, title, old_price, discount, rating,count,price }) => {
+  
+  
+  
+  
   return (
     <div className={styles.productCard}>
       <img className={styles.card_img} src={image} alt={title} />
       <p className={styles.title}>{title}</p>
       <div className={styles.priceclass}>
         <p className={styles.old_price}>Rs.{old_price}</p>
-        <p className={styles.price}>Rs.{price}</p>
-        <p className={styles.discount}>{discount}</p>
+        <p className={styles.price}>{price}</p>
+        <p>{discount}  </p>
       </div>
-      <p className={styles.rating}> {rating}</p>
+      <p className={styles.rating}> {rating}{(count)}</p>
     </div>
   );
 };
